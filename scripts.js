ab39c5cb0b69a9ea7ed122d5c091f6160f22197b
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-06 15:06:42 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Kamaljeet Kaur");
+$("#policyNumber").text("PDI6208480");
+$("#startDate").text("07-Aug-2020");
+$("#endDate").text("06-Aug-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08DU1049");
+$("#make").text("HONDA");
+$("#model").text("CD110 DREAM");
+$("#vehicalSubType").text("SELF");
+$("#engineNo").text("JC67E84037183");
+$("#chassis").text("ME4JC677FH8032626");
